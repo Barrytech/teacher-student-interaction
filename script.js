b198539a4a16546c49f935d04fa7e62ce33805b0
Barry <barry@citypolyhs.org>
2017-03-27 20:33:00 +0000

    i started the project

---
+++ script.js
@@ -0,0 +1,31 @@
+/*global$*/
+
+$(document).ready(function(){
+    
+    var TeachersArray=[ "Josh", "Thai", "Micheal", "David"];
+    console.log(TeacherArray[])
+    
+    var StudentsArray=["Barry", "Usman", "Jason", "Aldair", "Taufiq"]
+    console.log(StudentsArray[])
+   
+    var Randomnum= Math.rondom();
+    console.log(TeachersArry);
+    var TeachersArray= TeachersArray*6;
+    console.log(num6); 
+   return Math.floor(num6)+1;
+   
+   
+    var StudentsArray= Math.rondom();
+  console.log(StudentsArry);
+  var TeachersArray= StudentsArray*6;
+  console.log(StudentsArray);
+   return Math.floor(StudentsArray)+1;
+   
+    $("#button").click(function(){
+     
+     
+      
+      
+    };
+  
+});
