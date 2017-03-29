11106c1bc34b6fbe8120e8a0ee51936045006f0d
Barry <barry@citypolyhs.org>
2017-03-29 20:15:21 +0000

    well done

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 /*global$*/
 
 $(document).ready(function(){
+ 
+  function rolldice(x){
+  //Place your code here 
+ if(x==="Teacher") {
+  var limit=3;
+  
+ 
+ }
+ if (x==="Student"){
+  var limit=4;
+ }
+  var num= Math.random();
+  console.log(num);
+   num= num*limit;
+ 
+   return Math.floor(num);
     
+  }
+   
     var TeachersArray=[ "Josh", "Thai", "Micheal", "David"];
-    console.log(TeacherArray[])
+    console.log(TeachersArray[rolldice("Teacher")])
     
     var StudentsArray=["Barry", "Usman", "Jason", "Aldair", "Taufiq"]
-    console.log(StudentsArray[])
-   
-    var Randomnum= Math.rondom();
-    console.log(TeachersArry);
-    var TeachersArray= TeachersArray*6;
-    console.log(num6); 
-   return Math.floor(num6)+1;
-   
-   
-    var StudentsArray= Math.rondom();
-  console.log(StudentsArry);
-  var TeachersArray= StudentsArray*6;
-  console.log(StudentsArray);
-   return Math.floor(StudentsArray)+1;
-   
-    $("#button").click(function(){
+    console.log(StudentsArray[rolldice("Student")])
+  
+  
+    $("#tbutton").click(function(){
+     $("#teacher").html(TeachersArray[rolldice("Teacher")]);
+     
      
      
       
       
-    };
+    });
+    $("#sbutton").click(function(){
+     $("#student").html(StudentsArray[rolldice("Student")])
+     
+    });
   
 });
